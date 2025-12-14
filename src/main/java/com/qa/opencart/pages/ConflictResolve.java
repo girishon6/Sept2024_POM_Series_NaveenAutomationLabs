26ba4c7e386fb2d1ee3b9d26525b2de43008dcca
girishon6 <girishon6@gmail.com>
2025-12-14 17:23:32 +0530

    Create ConflictResolve.java

---
+++ src/main/java/com/qa/opencart/pages/ConflictResolve.java
@@ -0,0 +1,14 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+
+public class ConflictResolve {
+	
+
+	private By feature=By.id("ConflictResolveByDwight");
+	
+	public void LoginPage() {
+		System.out.println("ConflictResolveByDwight");
+	}
+
+}
