12c71b40b8b9a004fbdcc59d2503cb1f40ddd951
girishon6 <girishon6@gmail.com>
2025-12-14 17:20:58 +0530

    added loginPageDemo

--- src/main/java/com/qa/opencart/pages/LoginPageDemo.java
+++ src/main/java/com/qa/opencart/pages/LoginPageDemo.java
@@ -4,10 +4,18 @@ import org.openqa.selenium.By;
 
 public class LoginPageDemo {
 	
-	private By feature=By.id("LoginPageDemo");
+	/*
+	 * <<<<<<< Updated upstream private By feature=By.id("LoginPageDemo");
+	 * 
+	 * public void LoginPage() { System.out.println("LoginPage"); }
+	 * 
+	 * } =======
+	 */
+	private By feature=By.id("LoginPageDemoByGirish");
 	
 	public void LoginPage() {
-		System.out.println("LoginPage");
+		System.out.println("LoginPageByGirish");
 	}
 
 }
+/* >>>>>>> Stashed changes */
