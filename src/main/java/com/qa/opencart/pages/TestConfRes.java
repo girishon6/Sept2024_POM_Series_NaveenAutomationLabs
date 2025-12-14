0888ec1573c8f731efe461073660b1a0898a1129
girishon6 <girishon6@gmail.com>
2025-12-14 17:44:10 +0530

    Create TestConfRes.java

---
+++ src/main/java/com/qa/opencart/pages/TestConfRes.java
@@ -0,0 +1,15 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+
+public class TestConfRes {
+	
+
+	private By feature=By.id("TestConflictResolveByDwight");
+	
+	public void LoginPage() {
+		System.out.println("TestConflictResolveByDwight");
+
+
+}
+}
