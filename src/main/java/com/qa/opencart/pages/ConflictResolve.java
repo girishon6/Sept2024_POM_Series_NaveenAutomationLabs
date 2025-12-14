3b30815ae2fc75b691c413892759f0a7e9d86786
girishon6 <girishon6@gmail.com>
2025-12-14 17:26:09 +0530

    Adding conflict Resolve file

---
+++ src/main/java/com/qa/opencart/pages/ConflictResolve.java
@@ -0,0 +1,15 @@
+
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+
+public class ConflictResolve {
+	
+
+	private By feature=By.id("ConflictResolveByGirish");
+	
+	public void LoginPage() {
+		System.out.println("ConflictResolveByGirish");
+	}
+
+}
